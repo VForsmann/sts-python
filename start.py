3151fa21ef648343b5f6895de78b6732cb4831c4
Till Hoffmann <tillhoffmann1411@gmail.com>
2018-11-27 20:17:58 +0100

    added first histogramm

--- start.py
+++ start.py
@@ -1,4 +1,14 @@
 import data_preparation.csv_import as csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Start script
 data = csv.load_data()
+
+f22_data = np.array(data[['f22']])
+median = np.median(f22_data[f22_data > 0])
+f22_data[f22_data == 0] = median
+print(f22_data)
+pd.DataFrame.hist(pd.DataFrame(f22_data))
+plt.show()
