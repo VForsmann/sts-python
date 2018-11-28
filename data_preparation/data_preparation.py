8da2ac1a8520761d296515dbde709307592a7212
Timo Reimer <tr804674@fh-muenster.de>
2018-11-28 13:29:13 +0100

    Changed location

--- data_preparation/data_preparation.py
+++ data_preparation/data_preparation.py
@@ -4,51 +4,7 @@ import matplotlib.pyplot as plt
 import functions as fn
 
 data = fn.load_data('../daten_robinson.csv')
-
-
-# count the values and how often they appear
-def count_values(field):
-    unique, counts = np.unique(data[field], return_counts=True)
-    rows = dict(zip(unique, counts))
-    print(rows)
-
-
-# replace its given value with zero
-def set_empty_values_new(field, replace_value=' '):
-    data[field] = data[field].replace(replace_value, '0')
-    print(data[field])
-
-
-# replace a value zero to median of the field
-def set_zero_to_median(field):
-    answer = np.array(data[[field]])
-    median = np.median(answer[answer > 0])
-    answer[answer == 0] = median
-    data[field] = answer
-
-
-# replace every zero value with the median
-def clean_years():
-    f22_data = np.array(data[['f22']])
-    median = np.median(f22_data[f22_data > 0])
-    f22_data[f22_data == 0] = median
-    pd.DataFrame.hist(pd.DataFrame(f22_data))
-
-
-def descriptive_statistics():
-    data.describe()
-
-
-descriptive_statistics()
-
-
-# replace a field value with a given new value
-def set_field_value_to_new_value(field, replace_value, new_value):
-    data[field] = data[field].replace(replace_value, new_value)
-    print(data[field])
-
-
-set_zero_to_median('f22')
+print(fn.set_zero_to_median(data, 'f22'))
 # set_empty_values_new('f17_Comment')
 # set_empty_values_new('f17', replace_value='-99')
 # set_field_value_to_new_value('f1', 0, 1)
--- functions.py
+++ functions.py
@@ -10,3 +10,35 @@ def sum_characteristics(data: np.numarray):
 
 def load_data(file):
     return pd.read_csv(file, sep=';', na_values='Nothing', error_bad_lines=False)
+
+
+# count the values and how often they appear
+def count_values(data, field):
+    unique, counts = np.unique(data[field], return_counts=True)
+    rows = dict(zip(unique, counts))
+    return rows
+
+
+# replace its given value with zero
+def set_empty_values_new(data, field, replace_value=' '):
+    data[field] = data[field].replace(replace_value, '0')
+    return data[field]
+
+
+# replace a value zero to median of the field
+def set_zero_to_median(data, field):
+    answer = np.array(data[[field]])
+    median = np.median(answer[answer > 0])
+    answer[answer == 0] = median
+    data[field] = answer
+    return data[field]
+
+
+def descriptive_statistics(data):
+    return data.describe()
+
+
+# replace a field value with a given new value
+def set_field_value_to_new_value(data, field, replace_value, new_value):
+    data[field] = data[field].replace(replace_value, new_value)
+    return data[field]
