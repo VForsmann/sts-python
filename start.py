306d640682f4de55f865d3b7971126c65e083609
Till Hoffmann <tillhoffmann1411@gmail.com>
2018-11-28 16:38:10 +0100

    show logos in scatter

--- start.py
+++ start.py
@@ -1,4 +1,3 @@
-import data_preparation.csv_import as csv
 import functions as fn
 import numpy as np
 import pandas as pd
@@ -12,6 +11,6 @@ logos = data[['f12_1', 'f12_2', 'f12_3', 'f12_4', 'f12_5', 'f12_6', 'f12_7', 'f1
 sum_logos = fn.sum_characteristics(np.array(logos[1:]))
 
 pd_logos = pd.DataFrame(sum_logos)
-print(logos)
-plt.hist(logos)
+print(sum_logos)
+plt.scatter([1, 2, 3, 4, 5, 6, 7, 8, 9], sum_logos)
 plt.show()
