6ad6f217f5d09ab899fb83d23f6b7c22fb162e0c
VForsmann <vitus.forsmann@hotmail.de>
2018-12-11 22:21:50 +0100

    correlationsTable with clickEvent

--- correlations/correlation_f3.py
+++ correlations/correlation_f3.py
@@ -12,7 +12,7 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 
 x = pd.crosstab(data['f3_2'], data['f3_1'], rownames=['f2'], colnames=['f1'])
-corr = data.corr()
+corr = data.corr(method="spearman")
 corr = corr[(corr[:] > 0.5) & (corr[:] < 1)].dropna(how='all').dropna(axis='columns', how='all')
 print(corr)
 # table = go.Table(
@@ -28,6 +28,23 @@ print(corr)
 # table = [table]
 
 corr.to_html('pandas_table.html')
+
+html = open('pandas_table.html', 'a')
+
+html.write("""<script>
+let tds = document.getElementsByTagName("td");
+
+for (td of tds) {
+td.onclick = (event) => {
+alert("Spalte: " + getNameOfIndex(event.srcElement.cellIndex) + " Zeile: " + getNameOfIndex(event.srcElement.parentElement.rowIndex));
+}
+}
+function getNameOfIndex(index) {
+let ths = document.getElementsByTagName("th")
+return (ths[index].innerText);
+}
+</script>""")
+
 
 # py.offline.plot(table, filename='pandas_table.html')
 
