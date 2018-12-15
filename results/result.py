189a963543d72084341ac9ec081dcd0b14146331
Till Hoffmann <tillhoffmann1411@gmail.com>
2018-12-15 14:47:24 +0100

    finished third assigment

--- results/result.py
+++ results/result.py
@@ -21,3 +21,9 @@ regr.reg_for_prep_data(sport, well_being, data)
 print('xxxxxxxxxxxxxxxxxxxxxxxxxxx - 2. Assigment - xxxxxxxxxxxxxxxxxxxxxxxxxxx')
 # TODO hier noch die korrelationen einfügen, aber erst, wenn korrelation eine eigene funktion ist
 print(data['f4_13'][(data['f4_13'] < 8) & (data['f4_13'] > 0)].describe())
+
+# 3. Assignment
+print('xxxxxxxxxxxxxxxxxxxxxxxxxxx - 3. Assigment - xxxxxxxxxxxxxxxxxxxxxxxxxxx')
+# TODO hier noch ein plot erstellen, der pro frage den Mittelwert darstellt und ggf. irgendwie noch die std
+bio_elements = ['f15_1', 'f15_2', 'f15_3', 'f15_4', 'f15_5', 'f15_6', 'f15_7', 'f15_8', 'f15_9', 'f15_10', 'f15_11', 'f15_12', 'f15_13', 'f15_14', 'f15_15', 'f15_16']
+print(data[bio_elements][(data[bio_elements] < 8) & (data[bio_elements] > 0)].describe())
