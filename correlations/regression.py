06d5c0c220232b945bb3b3ca99d975ee61733a4c
Till Hoffmann <tillhoffmann1411@gmail.com>
2018-12-15 11:54:46 +0100

    finished regression

--- correlations/regression.py
+++ correlations/regression.py
@@ -5,35 +5,43 @@ from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 import functions as fn
 
-data = fn.load_data('../data_preparation/data_preparation.csv')
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-X = data[['f4_13', 'f8', 'f9', 'f10_1', 'f10_2', 'f15_8', 'f16_3', 'f16_8', 'f18_2']]
-Y = data['f18_7']
 
-# sklearn
-regr = linear_model.LinearRegression()
-regr.fit(X, Y)
+def reg_for_prep_data(x, y):
+    data = fn.load_data('../data_preparation/data_preparation.csv')
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    x = data[x]
+    y = data[y]
 
-print('Intercept: \n', regr.intercept_)
-print('Coefficients: \n', regr.coef_)
+    # sklearn
+    regr = linear_model.LinearRegression()
+    regr.fit(x, y)
 
-# statsmodels
-X = sm.add_constant(X)
+    print('Intercept: \n', regr.intercept_)
+    print('Coefficients: \n', regr.coef_)
 
-factor = []
-header = []
-for i in range(0, X.shape[1]):
-    factor.append(variance_inflation_factor(X.values, i))
-    header = X.columns
-vif = pd.DataFrame()
-vif = vif.append(pd.Series(factor), ignore_index=True)
-vif.columns = pd.Series(header)
-vif.rename(index={0: 'VIF Factor', 1: 'Header'}, inplace=True)
-print(vif.transpose())
+    # statsmodels
+    x = sm.add_constant(x)
 
-model = sm.OLS(Y, X).fit()
-predictions = model.predict(X)
+    factor = []
+    header = []
+    for i in range(0, x.shape[1]):
+        factor.append(variance_inflation_factor(x.values, i))
+        header = x.columns
+    vif = pd.DataFrame()
+    vif = vif.append(pd.Series(factor), ignore_index=True)
+    vif.columns = pd.Series(header)
+    vif.rename(index={0: 'VIF Factor'}, inplace=True)
+    print(vif.transpose())
 
-print_model = model.summary()
-print(print_model)
+    model = sm.OLS(y, x).fit()
+    predictions = model.predict(x)
+
+    print_model = model.summary()
+    print(print_model)
+
+
+foo = ['f4_13', 'f8', 'f9', 'f10_1', 'f10_2', 'f15_8', 'f16_3', 'f16_8', 'f18_2']
+bar = 'f18_7'
+reg_for_prep_data(foo, bar)
+
