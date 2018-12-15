15140fef74790b8ac4ebd6d724fecd53b8dfa114
Till Hoffmann <tillhoffmann1411@gmail.com>
2018-12-15 17:47:04 +0100

    started sixth assignment

--- correlations/correlation.py
+++ correlations/correlation.py
@@ -9,8 +9,8 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 
 pd.crosstab(data['f3_2'], data['f3_1'], rownames=['f2'], colnames=['f1'])
-
-filtered_data = data[['f4_13', 'f5_7', 'f5_8', 'f5_10', 'f8', 'f9', 'f10_1', 'f10_2', 'f17', 'f18_2', 'f18_3', 'f18_7', 'f18_9', 'f26']]
+# ['f4_13', 'f5_7', 'f5_8', 'f5_10', 'f8', 'f9', 'f10_1', 'f10_2', 'f17', 'f18_2', 'f18_3', 'f18_7', 'f18_9', 'f26']
+filtered_data = data
 corr = filtered_data.corr(method='spearman')
 filtered_corr = corr[((corr[:] > 0.4) & (corr[:] < 1)) | ((corr[:] < -0.5) & (corr[:] >= -1))].dropna(how='all').dropna(axis='columns', how='all')
 plt.matshow(corr)
