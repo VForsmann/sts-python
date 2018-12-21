07281981069fbc25b9d45951eb2f85c6ecabcf82
VForsmann <vitus.forsmann@hotmail.de>
2018-12-21 01:15:22 +0100

    Angular Entwicklung; Sidebar automatisiert; Dokumentation eingeführt

---
+++ sts/src/README.md
@@ -0,0 +1,22 @@
+# Interna
+
+## Visualisierung
+
+### Graphen
+
+Ein Graph kann mit der `<graph-component>` Komponente dargestellt werden.
+
+`<graph-component [data]="Name.html [height]="'500px'" [widht]="'500px'"></graph-component>`
+
+Die Name.html muss ein durch Python generierter HTML Graph im Verzeichnis: `sts_fallstudie__python/graphs/html/htmlGraphs` sein
+`height` und `width` sind normale im String gehaltene CSS Propertys, welche für korrekte Darstellung übergeben werden sollten.
+### Sidebar und Navigation
+
+Die Sidebar agiert komplett automatisch.
+Wenn ein Element Teil der Sidebar werden soll (Hierzu am besten Textelemente wie `h1` verwenden), dann kann dem Element die Klasse `navPoint-l` oder `navPoint-sm` gegeben werden. Welche Klasse bestimmt ob "Überpunkt" oder "Unterpunkt".
+
+`<h1 class="navPoint-l">Einführung</h1>`
+
+sorgt für einen automatischen Eintrag in der Sidebar.
+
+Navigieren per klick ist noch nicht zu 100% Einsatzfähig.
--- sts/src/app/components/sidebar/sidebar.component.ts
+++ sts/src/app/components/sidebar/sidebar.component.ts
@@ -8,22 +8,40 @@ import {Component, OnInit} from '@angular/core';
 export class SidebarComponent implements OnInit {
     underpoints = null;
     actualElem = null;
+    unterpunktL = "navPoint-l";
+    unterpunktSm = "navPoint-sm";
 
     constructor() {
     }
 
     ngOnInit() {
-        this.underpoints = document.getElementsByClassName("navPoint");
+        this.underpoints = document.getElementsByClassName(this.unterpunktL);
+        this.sortElements();
+
         this.getActualElement();
         window.onscroll = () => this.getActualElement();
     }
 
     getActualElement() {
         for (let i = 0; i < this.underpoints.length; i++) {
-            if (this.underpoints[i].getBoundingClientRect().y < (window.innerHeight / 2) && this.underpoints[i].getBoundingClientRect().y > 0) {
+            if (this.underpoints[i].getBoundingClientRect().y < window.innerHeight && this.underpoints[i].getBoundingClientRect().y >= 0) {
                 this.actualElem = this.underpoints[i];
                 break;
             }
         }
+    }
+
+    sortElements() {
+        let newUnderpoints = [];
+        for (let i = 0; i < this.underpoints.length; i++) {
+            this.underpoints[i].large = true;
+            newUnderpoints[newUnderpoints.length] = this.underpoints[i];
+            for (let y = 0; y < this.underpoints[i].parentNode.childNodes.length; y++) {
+                if (Array.from(this.underpoints[i].parentNode.childNodes[y].classList).includes(this.unterpunktSm)) {
+                    newUnderpoints[newUnderpoints.length] = this.underpoints[i].parentNode.childNodes[y];
+                }
+            }
+        }
+        this.underpoints = newUnderpoints;
     }
 }
