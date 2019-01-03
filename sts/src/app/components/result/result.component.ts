c5f2876ea50b97e85c0ab21a3c7dd39c1de6fc39
Timo Reimer <tr804674@fh-muenster.de>
2019-01-03 16:54:16 +0100

    Cluster finish

--- sts/src/app/components/result/result.component.ts
+++ sts/src/app/components/result/result.component.ts
@@ -20,6 +20,17 @@ export class ResultComponent implements OnInit {
     {question: 'f15_8', value: 1.190337 },
     {question: 'f18_2', value: 1.356053 }
   ];
+  headElementsKaiser = ['Frage', 'Kaiserkriterium'];
+  elementsKaiser = [
+    {question: 'f4_12', value:  0.812848 },
+    {question: 'f4_13', value: 0.832165 },
+    {question: 'f5_7', value: 0.879840 },
+    {question: 'f10_1', value: 0.873172 },
+    {question: 'f10_2', value: 0.823980 },
+    {question: 'f18_7', value: 0.902452 },
+    {question: 'f18_2', value: 0.898619 },
+    {question: 'f19_8', value: 0.824405 }
+  ];
   ngOnInit() {
   }
 
