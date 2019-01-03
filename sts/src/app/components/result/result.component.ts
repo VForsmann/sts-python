e69d616e5e4301a15207a1ed3f9c4907dfc24e4b
Till Hoffmann <tillhoffmann1411@gmail.com>
2019-01-03 18:17:10 +0100

    small fixes

--- sts/src/app/components/result/result.component.ts
+++ sts/src/app/components/result/result.component.ts
@@ -11,25 +11,25 @@ export class ResultComponent implements OnInit {
 
   headElements = ['Frage', 'VIF-Wert'];
   elements = [
-    {question: 'const', value:  9.552134 },
-    {question: 'f4_13', value: 1.937688 },
-    {question: 'f8', value: 2.413905 },
-    {question: 'f9', value: 2.320327 },
-    {question: 'f10_1', value: 2.436579 },
-    {question: 'f10_2', value: 1.662007 },
-    {question: 'f15_8', value: 1.190337 },
-    {question: 'f18_2', value: 1.356053 }
+    {question: 'const', value:  9.5521 },
+    {question: 'f4_13', value: 1.9377 },
+    {question: 'f8', value: 2.4139 },
+    {question: 'f9', value: 2.3203 },
+    {question: 'f10_1', value: 2.4366 },
+    {question: 'f10_2', value: 1.6620 },
+    {question: 'f15_8', value: 1.1903 },
+    {question: 'f18_2', value: 1.3560 }
   ];
   headElementsKaiser = ['Frage', 'Kaiserkriterium'];
   elementsKaiser = [
-    {question: 'f4_12', value:  0.812848 },
-    {question: 'f4_13', value: 0.832165 },
-    {question: 'f5_7', value: 0.879840 },
-    {question: 'f10_1', value: 0.873172 },
-    {question: 'f10_2', value: 0.823980 },
-    {question: 'f18_7', value: 0.902452 },
-    {question: 'f18_2', value: 0.898619 },
-    {question: 'f19_8', value: 0.824405 }
+    {question: 'f4_12', value:  0.8128 },
+    {question: 'f4_13', value: 0.8322 },
+    {question: 'f5_7', value: 0.8798 },
+    {question: 'f10_1', value: 0.8732 },
+    {question: 'f10_2', value: 0.8240 },
+    {question: 'f18_7', value: 0.9024 },
+    {question: 'f18_2', value: 0.8986 },
+    {question: 'f19_8', value: 0.8244 }
   ];
   ngOnInit() {
   }
